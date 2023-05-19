dc93114c7d0991586da98a9193fbd96143f556ab
ThePowerdinoDeluxe990 <118630450+ThePowerdinoDeluxe990@users.noreply.github.com>
2023-05-19 13:05:58 +0200

    Create cocos.dart

---
+++ cocos.dart
@@ -0,0 +1,20 @@
+class Point {
+  String? x; // Declare instance variable x, initially null.
+  String? y; // Declare y, initially null.
+}
+
+void cocos() {
+  var point = Point();
+  point.x = "Me pican";
+  point.y = "los cocos";
+  print(point.x);
+  print(point.y);
+}
+
+void ohio() {
+  var point = Point();
+  point.x = "Muchos dicen de que";
+  point.y = "Brrrrr";
+  print(point.x);
+  print(point.y);
+}
